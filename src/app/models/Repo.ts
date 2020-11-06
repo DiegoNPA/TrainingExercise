e0563de21bf61f47fdec337b2ae856186a787551
Diego <diego.penaranda@ucb.edu.bo>
2020-11-06 18:31:28 -0400

    Added both User and Repo model

---
+++ src/app/models/Repo.ts
@@ -0,0 +1,8 @@
+export class Repo {
+  repoLink: string;
+  directoryName: string;
+  description: string;
+  issues: number;
+  openIssues: number;
+  forks: number;
+}
---
+++ src/app/models/User.ts
@@ -0,0 +1,5 @@
+export class User {
+  userName: string;
+  gitHubLink: string;
+  avatar: string;
+}
