ae9ebcd663a9be0e922dbb4b73138d49bdbe4baa
Diego <diego.penaranda@ucb.edu.bo>
2020-11-08 11:01:34 -0400

    Now the initialization of the users verifies the localstorage and if the users object is stored and it was stored less than two hours ago, it retrieves it from there

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -14,10 +14,29 @@ export class UsersComponent implements OnInit {
   constructor(private userService: UsersService) { }
 
   ngOnInit(): void {
+
     this.userService.getUsers().subscribe(users => {
-      this.users = users;
-      console.log(users);
+      const hrs = 2;
+      var now = new Date().getTime();
+      var nowStr = JSON.stringify(now);
+      var setUpTime = localStorage.getItem('setUpTime');
+      if (setUpTime === null){
+        localStorage.setItem('setupTime', nowStr);
+        localStorage.setItem('users', JSON.stringify(users));
+        this.users = users;
+      }else{
+        if((now - JSON.parse(setUpTime)) > hrs*60*60*1000){
+          localStorage.clear()
+          localStorage.setItem('setupTime', nowStr);
+          localStorage.setItem('users', JSON.stringify(users));
+          this.users = users;
+        }
+        else{
+          this.users = JSON.parse(localStorage.getItem('users'));
+        }
+      }
     })
+
   }
 
 }
